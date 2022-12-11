30debc568c9dd8d485b0063d8717ab19af517995
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2022-12-11 19:55:26 +0700

    Add Edit Delete

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import *
 import pandas as pd
 import sqlite3 as sql
-from Route.ShowData import showdata
 
 app = Flask(__name__)
 app.secret_key = 'MjkwOQ=='
-app.register_blueprint(showdata)
 
 def dbConnect():
     try:
@@ -40,5 +38,59 @@ def logout():
     session.clear()
     return render_template("index.html")
 
+@app.route('/showdata')
+def showdata():
+    if session.get("username") == None:
+        return redirect(url_for("index"))
+    else:
+        conn = dbConnect()[0]
+        sql = "SELECT * FROM main.users"
+        df = pd.read_sql(sql , conn)
+        mydict = df.to_dict('records')
+        return render_template("showdata.html", data=mydict)
+
+@app.route('/edit/<id_data>')
+def edit(id_data):
+    conn = dbConnect()[0]
+    sql = f"SELECT * FROM users where id = {id_data}"
+    df = pd.read_sql(sql , conn)
+    mydict = df.to_dict('records')
+    return render_template("edit.html", data=mydict)
+
+@app.route('/editdata', methods=['POST'])
+def editdata():
+    conn = dbConnect()[0]
+    fname = request.form['fname']
+    lname = request.form['lname']
+    nickname = request.form['nickname']
+    id = request.form['id']
+    sql = f"UPDATE users SET fname = '{fname}', lname = '{lname}', nickname = '{nickname}' WHERE id = {id}"
+    conn.execute(sql)
+    conn.commit()
+    return redirect(url_for('showdata'))
+
+@app.route('/delete/<id_data>')
+def delete(id_data):
+    conn = dbConnect()[0]
+    conn.execute(f"DELETE from users where id = {id_data}")
+    conn.commit()
+    return redirect(url_for('showdata'))
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/adddata', methods=['POST'])
+def adddata():
+    conn = dbConnect()[0]
+    username = request.form['username']
+    password = request.form['password']
+    fname = request.form['fname']
+    lname = request.form['lname']
+    nickname = request.form['nickname']
+    conn.execute(f"INSERT INTO users (username,password,fname,lname,nickname) VALUES ('{username}','{password}','{fname}','{lname}','{nickname}')")
+    conn.commit()
+    flash("Save Data Successfully!")
+    return render_template('add.html')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
