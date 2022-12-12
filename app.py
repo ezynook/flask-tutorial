7828f4a12386be655e70387f451a09fd33ff37b2
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2022-12-12 11:49:05 +0700

    Replace sqlite lib to SQLAlchemy Lib

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import *
 import pandas as pd
-import sqlite3 as sql
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.secret_key = 'MjkwOQ=='
@@ -8,9 +8,8 @@ app.secret_key = 'MjkwOQ=='
 #Database Connect
 def dbConnect():
     try:
-        con = sql.connect("flask.db")
-        cur = con.cursor()
-        return [con, cur]
+        engine = create_engine('sqlite:///flask.db')
+        return engine
     except Exception as e:
         print('Error -> ', e)
 
@@ -25,12 +24,12 @@ def index():
 #Login
 @app.route("/home", methods=["POST"])
 def login():
-    cur = dbConnect()[1]
+    cur = dbConnect()
     username = request.form.get('username')
     password = request.form.get('password')
     statement = f"SELECT username, password from users WHERE username='{username}' AND password = '{password}';"
-    cur.execute(statement)
-    if not cur.fetchone():
+    rows = cur.execute(statement).fetchone()
+    if not rows:
         return render_template("index.html", data="Login Failed")
     else:
         session['username'] = username
@@ -44,7 +43,7 @@ def signup_page():
 #Signup
 @app.route("/signup", methods=['POST'])
 def signup():
-    conn = dbConnect()[0]
+    conn = dbConnect()
     username = request.form['username']
     password = request.form['password']
     fname = request.form['fname']
@@ -61,7 +60,7 @@ def showdata():
     if session.get("username") == None:
         return redirect(url_for("index"))
     else:
-        conn = dbConnect()[0]
+        conn = dbConnect()
         sql = "SELECT * FROM main.users"
         df = pd.read_sql(sql , conn)
         mydict = df.to_dict('records')
@@ -74,7 +73,7 @@ def add():
 #Add Data
 @app.route('/adddata', methods=['POST'])
 def adddata():
-    conn = dbConnect()[0]
+    conn = dbConnect()
     username = request.form['username']
     password = request.form['password']
     fname = request.form['fname']
@@ -87,7 +86,7 @@ def adddata():
 #Edit page
 @app.route('/edit/<id_data>')
 def edit(id_data):
-    conn = dbConnect()[0]
+    conn = dbConnect()
     sql = f"SELECT * FROM users where id = {id_data}"
     df = pd.read_sql(sql , conn)
     mydict = df.to_dict('records')
@@ -95,7 +94,7 @@ def edit(id_data):
 #Edit Data
 @app.route('/editdata', methods=['POST'])
 def editdata():
-    conn = dbConnect()[0]
+    conn = dbConnect()
     fname = request.form['fname']
     lname = request.form['lname']
     nickname = request.form['nickname']
@@ -107,7 +106,7 @@ def editdata():
 #Delete
 @app.route('/delete/<id_data>')
 def delete(id_data):
-    conn = dbConnect()[0]
+    conn = dbConnect()
     conn.execute(f"DELETE from users where id = {id_data}")
     conn.commit()
     return redirect(url_for('showdata'))
