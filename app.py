e667dfa3f14d6896fbe0a0d3487a5e941f1f4570
พสิษฐ์ ยอดสร้อย <ezynook@gmail.com>
2022-12-13 18:36:22 +0700

    Add Pagination

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from flask import *
+import os
 import pandas as pd
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'MjkwOQ=='
+BASEPATH = os.path.abspath(os.getcwd())
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASEPATH}/flask.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#Database Connect
-def dbConnect():
-    try:
-        engine = create_engine('sqlite:///flask.db')
-        return engine
-    except Exception as e:
-        print('Error -> ', e)
+db = SQLAlchemy(app)
+
+class users(db.Model):
+   __tablename__ = 'users'
+   id = db.Column(db.Integer, primary_key = True)
+   username = db.Column(db.String(50))
+   password = db.Column(db.String(20))  
+   fname = db.Column(db.String(20))  
+   lname = db.Column(db.String(20))  
+   nickname = db.Column(db.String(20))
+
+def __init__(self, id, username, password,fname, lname, nickname):
+   self.id = id
+   self.username = username
+   self.password = password
+   self.fname = fname
+   self.lname = lname
+   self.nickname = nickname
 
 #Index Page
 @app.route("/")
@@ -24,16 +39,14 @@ def index():
 #Login
 @app.route("/home", methods=["POST"])
 def login():
-    cur = dbConnect()
-    username = request.form.get('username')
-    password = request.form.get('password')
-    statement = f"SELECT username, password from users WHERE username='{username}' AND password = '{password}';"
-    rows = cur.execute(statement).fetchone()
-    if not rows:
-        return render_template("index.html", data="Login Failed")
+    c_username = request.form.get('username')
+    c_password = request.form.get('password')
+    check = users.query.filter_by(username = c_username, password = c_password).all()
+    if not check:
+        flash('Login Failure')
     else:
-        session['username'] = username
-        return redirect(url_for('showdata'))
+        session['username'] = c_username
+        return redirect(url_for('showdata', page_num=1))
 
 #Signup-page
 @app.route("/signup-page")
@@ -43,28 +56,29 @@ def signup_page():
 #Signup
 @app.route("/signup", methods=['POST'])
 def signup():
-    conn = dbConnect()
-    username = request.form['username']
-    password = request.form['password']
-    fname = request.form['fname']
-    lname = request.form['lname']
-    nickname = request.form['nickname']
-    conn.execute(f"INSERT INTO users (username,password,fname,lname,nickname) VALUES ('{username}','{password}','{fname}','{lname}','{nickname}')")
-    conn.commit()
+    user = users(
+        username = request.form['username'],
+        password = request.form['password'],
+        fname = request.form['fname'],
+        lname = request.form['lname'],
+        nickname = request.form['nickname']
+    )
+    db.session.add(user)
+    db.session.commit()
     flash("Signup Successfully!")
     return render_template('index.html')
 
 #Show Data
-@app.route('/showdata')
-def showdata():
+@app.route('/showdata/<int:page_num>')
+def showdata(page_num):
+
     if session.get("username") == None:
         return redirect(url_for("index"))
     else:
-        conn = dbConnect()
-        sql = "SELECT * FROM main.users"
-        df = pd.read_sql(sql , conn)
-        mydict = df.to_dict('records')
-        return render_template("showdata.html", data=mydict, session=session['username'])
+        
+        result = users.query.paginate(per_page=3, page=page_num, error_out=False)
+        
+        return render_template("showdata.html", data=result, session=session['username'],pagelength=page_num)
 
 #Add Page
 @app.route('/add')
@@ -73,42 +87,36 @@ def add():
 #Add Data
 @app.route('/adddata', methods=['POST'])
 def adddata():
-    conn = dbConnect()
-    username = request.form['username']
-    password = request.form['password']
-    fname = request.form['fname']
-    lname = request.form['lname']
-    nickname = request.form['nickname']
-    conn.execute(f"INSERT INTO users (username,password,fname,lname,nickname) VALUES ('{username}','{password}','{fname}','{lname}','{nickname}')")
-    conn.commit()
+    user = users(
+        username = request.form['username'],
+        password = request.form['password'],
+        fname = request.form['fname'],
+        lname = request.form['lname'],
+        nickname = request.form['nickname']
+    )
+    db.session.add(user)
+    db.session.commit()
     flash("Save Data Successfully!")
     return render_template('add.html')
 #Edit page
 @app.route('/edit/<id_data>')
 def edit(id_data):
-    conn = dbConnect()
-    sql = f"SELECT * FROM users where id = {id_data}"
-    df = pd.read_sql(sql , conn)
-    mydict = df.to_dict('records')
-    return render_template("edit.html", data=mydict)
+    sql = users.query.filter_by(id = id_data).all()
+    return render_template("edit.html", data=sql)
 #Edit Data
 @app.route('/editdata', methods=['POST'])
 def editdata():
-    conn = dbConnect()
-    fname = request.form['fname']
-    lname = request.form['lname']
-    nickname = request.form['nickname']
-    id = request.form['id']
-    sql = f"UPDATE users SET fname = '{fname}', lname = '{lname}', nickname = '{nickname}' WHERE id = {id}"
-    conn.execute(sql)
-    conn.commit()
+    item = users.query.get(request.form['id'])
+    item.fname = request.form['fname']
+    item.lname = request.form['lname']
+    item.nickname = request.form['nickname']
+    db.session.commit()
     return redirect(url_for('showdata'))
 #Delete
 @app.route('/delete/<id_data>')
 def delete(id_data):
-    conn = dbConnect()
-    conn.execute(f"DELETE from users where id = {id_data}")
-    conn.commit()
+    users.query.filter_by(id=id_data).delete()
+    db.session.commit()
     return redirect(url_for('showdata'))
 
 #Logout
@@ -116,6 +124,7 @@ def delete(id_data):
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
